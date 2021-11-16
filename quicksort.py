56732337d6e121ee7b1ad9b0b2335e1e0d2965ad
wym <752419921@qq.com>
2021-11-16 15:11:25 +0800

    push quicksort

---
+++ quicksort.py
@@ -0,0 +1,23 @@
+def getMiddle(nums, low, high)->int:
+    temp = nums[low]
+    while(low < high):
+        while(nums[low] < temp):
+            low+=1
+        while(nums[high] > temp):
+            high-=1
+        nums[low], nums[high] = nums[high], nums[low]
+    nums[low] = temp
+    return low
+
+def quickSort(nums, low, high):
+    if(low < high):
+        middle = getMiddle(nums, low, high)
+        quickSort(nums, low, middle - 1)
+        quickSort(nums, middle + 1,  high)
+
+nums = [1,2,5,0,6,7]
+quickSort(nums, 0, 5)
+print("nums =",nums)
+
+
+
